2390d32ef893330456e145152c2452b30fe4a1bf
Natawut Nupairoj <natawut.n@chula.ac.th>
2024-10-20 17:15:22 +0700

    add terraform docker example

---
+++ terraform/docker/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.21.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
+
+output "nginx_id" {
+  value = docker_container.nginx.id
+}
+
+output "nginx_ip_address" {
+  value = docker_container.nginx.network_data[0].ip_address
+}
